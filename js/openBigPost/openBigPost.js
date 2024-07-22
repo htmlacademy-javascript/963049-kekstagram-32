6c332608030406ff5d86b78cbfb922da926fa5ff
Enver-B <70843623+Enver-B@users.noreply.github.com>
2024-07-22 17:03:35 +0300

    Задание 8. Открывается и закрывается(8.14 и частично 8.15)

--- js/openBigPost/openBigPost.js
+++ js/openBigPost/openBigPost.js
@@ -1,84 +1,86 @@
-import '../createUserPosts/createUserPosts.js';
-import {createСomment} from '../createDataPosts/createDataPosts.js';
-import {isEscapeKey} from '../util.js';
+//КОД, В КОТОРОМ РАБОТАЮ
+const bigPicture = document.querySelector('.big-picture');
+const body = document.querySelector('body');
 
-const userBigPost = document.querySelector('.big-picture');
-const openUserPosts = document.querySelector('.pictures');
-const buttonCloseBigPicture = document.querySelector('.big-picture__cancel');
-const socialItemCommentCount = document.querySelector('.social__comment-count');
-const socialCommentLoader = document.querySelector('.comments-loader');
+const closeBigPicture = () => bigPicture.classList.add('hidden');
 
 //Создание списка комментариев под постом
 
-const renderSocialComments = () => {
-  const socialListComment = document.querySelector('.social__comments');
-  const socialItemComment = socialListComment.querySelector('.social__comment');
-  const socialComment = createСomment();
+const socialCommentList = document.querySelector('.social__comments');
+const socialCommentItem = socialCommentList.querySelector('.social__comment');
+const socialItemCommentCount = document.querySelector('.social__comment-count');
+const socialCommentLoader = document.querySelector('.comments-loader');
+const socialCommenTotalCount = document.querySelector('.social__comment-total-count');
+//const socialCommenShownCount = document.querySelector('.social__comment-shown-count');
 
-  const SOCIAL_PICTURE_WIDTH = 35;
-  const SOCIAL_PICTURE_HEIGHT = 35;
+const SOCIAL_PICTURE_WIDTH = 35;
+const SOCIAL_PICTURE_HEIGHT = 35;
 
-  const socialListFragment = document.createDocumentFragment();
+let commentsData = [];
+let commentsStartIndex = 0;
+let commentsShownCount = 0;
+const SHOWN_COMMENTS = 5;
 
-  socialComment.forEach(({avatar, name, message}) => {
-    const socialCommentElement = socialItemComment.cloneNode(true);
-    socialCommentElement.querySelector('.social__picture').src = avatar;
-    socialCommentElement.querySelector('.social__picture').alt = name;
-    socialCommentElement.querySelector('.social__picture').width = SOCIAL_PICTURE_WIDTH;
-    socialCommentElement.querySelector('.social__picture').height = SOCIAL_PICTURE_HEIGHT;
-    socialCommentElement.querySelector('.social__text').textContent = message;
+const getComment = ({avatar, name, message}) => {
+  const socialCommentElement = socialCommentItem.cloneNode(true);
+  socialCommentElement.querySelector('.social__picture').src = avatar;
+  socialCommentElement.querySelector('.social__picture').alt = name;
+  socialCommentElement.querySelector('.social__picture').width = SOCIAL_PICTURE_WIDTH;
+  socialCommentElement.querySelector('.social__picture').height = SOCIAL_PICTURE_HEIGHT;
+  socialCommentElement.querySelector('.social__text').textContent = message;
+  return socialCommentElement;
+};
 
-    socialListFragment.appendChild(socialCommentElement);
+const addComments = (comments) => {
+  const commentFragment = document.createDocumentFragment();
+  comments.forEach((comment) => {
+    commentFragment.append(getComment(comment));
   });
 
-  socialListComment.appendChild(socialListFragment);
+  commentsShownCount += comments.length;
+  socialCommenTotalCount.textContent = commentsShownCount;
+  socialItemCommentCount.textContent = `${commentsShownCount} из ${commentsData.length} комментариев`;
+
+  if(commentsShownCount >= commentsData.length) {
+    socialCommentLoader.classList.add('hidden');
+  }
+
+  socialCommentList.append(commentFragment);
 };
 
-/*
-const SHOWN_COMMENTS = 5;
-const commentStartIndex = 0;
 
-const shownCommentsList = () => {
-  renderSocialComments().slice(commentStartIndex, commentStartIndex + SHOWN_COMMENTS);
+const showComments = () => {
+  const comments = commentsData.slice(commentsStartIndex, commentsStartIndex + SHOWN_COMMENTS);
+  commentsStartIndex += SHOWN_COMMENTS;
+  addComments(comments);
 };
-*/
 
 //Открытие поста на весь экран
-const getBigPostData = ({url, likes, comments, description}) => {
-  userBigPost.querySelector('.big-picture__img img').src = url;
-  userBigPost.querySelector('.likes-count').textContent = likes;
-  renderSocialComments.textContent = comments.length;
-  userBigPost.querySelector('.social__caption').textContent = description;
+
+const openBigPicture = ({url, description, likes, comments}) => {
+  bigPicture.querySelector('.big-picture__img img').src = url;
+  bigPicture.querySelector('.social__caption').textContent = description;
+  bigPicture.querySelector('.likes-count').textContent = likes;
+  commentsData = comments;
+  socialCommentLoader.addEventListener('click', showComments);
+  bigPicture.classList.remove('hidden');
+
+  socialCommentList.innerHTML = '';
+  showComments();
+  // socialItemCommentCount.classList.add('hidden');
+  // socialCommentLoader.classList.add('hidden');
+  body.classList.add('modal-open');
+
+  document.addEventListener('keydown', (e) => {
+    if(e.keyCode === 27) {
+      closeBigPicture();
+    }
+    body.classList.remove('modal-open');
+  });
+  bigPicture.querySelector('.big-picture__cancel').addEventListener('click', () => {
+    closeBigPicture();
+    body.classList.remove('modal-open');
+  });
 };
 
-const onDocumentKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
-    closeUserPost();
-  }
-};
-
-function openUserPost () {
-  userBigPost.classList.remove('hidden');
-
-  document.addEventListener('keydown', onDocumentKeydown);
-}
-
-function closeUserPost () {
-  userBigPost.classList.add('hidden');
-
-  document.removeEventListener('keydown', onDocumentKeydown);
-}
-
-openUserPosts.addEventListener('click', () => {
-  openUserPost();
-
-  socialItemCommentCount.classList.add('hidden');
-  socialCommentLoader.classList.add('hidden');
-});
-
-buttonCloseBigPicture.addEventListener('click', () => {
-  closeUserPost();
-});
-
-export {getBigPostData};
+export {openBigPicture};
